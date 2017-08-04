15737db6cd5c7e5546dc8274ee0226685eb0fa73
Mark <1120888325@qq.com>
2017-08-04 12:12:42 +0800

    新增考虑浏览器默认字体的方案demo_03

--- src/remLayout.js
+++ src/remLayout.js
@@ -1,9 +1,14 @@
+//不考虑dpr，如需考虑dpr使用淘宝的flexible.js 或添加demo_02里的dpr处理代码
+
+var designWidth = 750, rem2px = 100;
+var html = document.querySelector("html");
+//考虑android浏览器默认字体大小不是16px的情况，需要获取实际的字体大小
+var htmlFontSize = parseFloat(window.getComputedStyle(html, null).getPropertyValue('font-size'));
 function refreshRem() {
-	var baseWidth = 750;	//以750设计稿为基准  也可以以640设计稿为基准
-	var baseFontSize = 100;	//转换基数为100
-	var currentDeviceWidth = document.documentElement.getBoundingClientRect().width;
-	var currentFontSize = (currentDeviceWidth / baseWidth) * baseFontSize;
-	document.documentElement.setAttribute("style", "font-size:" + currentFontSize + "px;");
+	html.style.fontSize = html.getBoundingClientRect().width / designWidth * rem2px / htmlFontSize * 100 + '%';
 }
 refreshRem();
-window.addEventListener("resize", refreshRem);
+//window.resize检测用户设备横屏和竖屏
+window.addEventListener("resize", function(){
+	refreshRem();
+});
