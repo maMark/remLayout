718117c8d2703c2d9f57f6bb12131bf8c42eddaf
mashuiquan <mashuiquan@ad61v1.com>
2018-05-25 10:34:20 +0800

    将全局变量放入函数，减少变量污染

--- src/remLayout.js
+++ src/remLayout.js
@@ -1,10 +1,9 @@
 //不考虑dpr，如需考虑dpr使用淘宝的flexible.js 或添加demo_02里的dpr处理代码
-
-var designWidth = 750, rem2px = 100;
-var html = document.querySelector("html");
-//考虑android浏览器默认字体大小不是16px的情况，需要获取实际的字体大小
-var htmlFontSize = parseFloat(window.getComputedStyle(html, null).getPropertyValue('font-size'));
 function refreshRem() {
+	var designWidth = 750, rem2px = 100;
+	var html = document.querySelector("html");
+	//考虑android浏览器默认字体大小不是16px的情况，需要获取实际的字体大小
+	var htmlFontSize = parseFloat(window.getComputedStyle(html, null).getPropertyValue('font-size'));
 	html.style.fontSize = html.getBoundingClientRect().width / designWidth * rem2px / htmlFontSize * 100 + '%';
 }
 refreshRem();
