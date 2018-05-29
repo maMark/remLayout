5971ade6d53311d365785ba978889ac587a4d3f3
mashuiquan <mashuiquan@ad61v1.com>
2018-05-29 11:19:10 +0800

    撤销上次修改导致横竖屏切换时无法正确计算html字体大小

--- src/remLayout.js
+++ src/remLayout.js
@@ -1,9 +1,9 @@
 //不考虑dpr，如需考虑dpr使用淘宝的flexible.js 或添加demo_02里的dpr处理代码
-function refreshRem() {
-	var designWidth = 750, rem2px = 100;
-	var html = document.querySelector("html");
+var designWidth = 750, rem2px = 100;
+var html = document.querySelector("html");
 	//考虑android浏览器默认字体大小不是16px的情况，需要获取实际的字体大小
-	var htmlFontSize = parseFloat(window.getComputedStyle(html, null).getPropertyValue('font-size'));
+var htmlFontSize = parseFloat(window.getComputedStyle(html, null).getPropertyValue('font-size'));
+function refreshRem() {
 	html.style.fontSize = html.getBoundingClientRect().width / designWidth * rem2px / htmlFontSize * 100 + '%';
 }
 refreshRem();
