fef1f46c274c71463e09a1f96f1c06ad3812dfe1
Mark <1120888325@qq.com>
2018-11-19 10:30:51 +0800

    使用自执行函数，减少变量污染

--- src/remLayout.js
+++ src/remLayout.js
@@ -1,13 +1,15 @@
 //不考虑dpr，如需考虑dpr使用淘宝的flexible.js 或添加demo_02里的dpr处理代码
-var designWidth = 750, rem2px = 100;
-var html = document.querySelector("html");
-	//考虑android浏览器默认字体大小不是16px的情况，需要获取实际的字体大小
-var htmlFontSize = parseFloat(window.getComputedStyle(html, null).getPropertyValue('font-size'));
-function refreshRem() {
-	html.style.fontSize = html.getBoundingClientRect().width / designWidth * rem2px / htmlFontSize * 100 + '%';
-}
-refreshRem();
+;(function () {
+  var designWidth = 750, rem2px = 100;
+  var html = document.querySelector("html");
+  //考虑android浏览器默认字体大小不是16px的情况，需要获取实际的字体大小
+  var htmlFontSize = parseFloat(window.getComputedStyle(html, null).getPropertyValue('font-size'));
+  function refreshRem() {
+    html.style.fontSize = html.getBoundingClientRect().width / designWidth * rem2px / htmlFontSize * 100 + '%';
+  }
+  refreshRem();
 //window.resize检测用户设备横屏和竖屏
-window.addEventListener("resize", function(){
-	refreshRem();
-});
+  window.addEventListener("resize", function(){
+    refreshRem();
+  });
+})();
